3cb6493031d405df1e6c0de37e30ca985de6f190
Abdellah Archtouy <aarchtou@e3r7p11.1337.ma>
2025-07-16 13:41:35 +0100

    create loading (is a clone  function of tqdm ) function that output the progress of loading data or somethign else using percent

--- Starting/ex08/Loading.py
+++ Starting/ex08/Loading.py
@@ -1,18 +1,15 @@
-# from time import sleep
-# from tqdm import tqdm
+import sys
+import time
+
+def ft_tqdm(iterable):
+    total = len(iterable)
+    for i in iterable:
+        percent = int(((i + 1) / total) * 100)
+        bar = '=' * (percent // 2) + '-' * (50 - percent // 2)
+        print(f"\r{percent}% [{bar}] {i + 1}/{total}", end="")
+        yield i
 
 
-# def ft_tqdm(lst: range) -> None:
-#     """A simple tqdm-like function to simulate progress bar."""
-#     total = len(lst)
-#     for i, item in enumerate(lst):
-#         percent = (i + 1) / total * 100
-#         print(f"\rProgress: {percent:.2f}%", end="")
-#     print()  # New line at the end
-
-# for elem in ft_tqdm(range(333)):
-#     sleep(0.005)
-# print()
-# for elem in tqdm(range(333)):
-#     sleep(0.005)
-# print()
+for i in ft_tqdm(range(100)):
+    time.sleep(0.05)
+print()
